1fbfbe4888f1bba1391b30a5ad2270de20996c31
Roman Shtylman <shtylman@gmail.com>
2013-02-17 23:33:53 -0500

    add remaining core modules
    - add core modules provided by -browserify packages

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ var resv = require('resolve');
 // core modules replaced by their browser capable counterparts
 var core = {};
 
+// load core modules from builtin dir
 fs.readdirSync(__dirname + '/builtin/').forEach(function(file) {
     core[path.basename(file, '.js')] = path.join(__dirname, '/builtin/', file);
 });
+
+// manually add core which are provided by modules
+core['http'] = require.resolve('http-browserify');
+core['vm'] = require.resolve('vm-browserify');
+core['crypto'] = require.resolve('crypto-browserify');
+core['console'] = require.resolve('console-browserify');
+core['zlib'] = require.resolve('zlib-browserify');
+core['buffer'] = require.resolve('buffer-browserify');
 
 function resolve(id, parent, cb) {
 
