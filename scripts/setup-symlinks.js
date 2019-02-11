dd08cbf23dbac3a6f1a733b54e303c3bd7eaa68a
Leo Horie <lhorie@uber.com>
2019-02-11 10:05:22 -0800

    create dir if it doesn't exist in ci

--- scripts/setup-symlinks.js
+++ scripts/setup-symlinks.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 
+try {
+  fs.mkdirSync(__dirname + '/../test/fixtures/node_modules/linker/node_modules');
+} catch (e) {}
 process.chdir(__dirname + '/../test/fixtures/node_modules/linker/node_modules');
 fs.unlinkSync('linked');
 fs.symlinkSync('../../../linked', 'linked', 'dir');
